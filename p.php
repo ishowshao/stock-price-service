f819f5f49824e585c742a7870c797536ad01a41c
ishowshao <ishowshao@me.com>
2016-02-22 15:12:06 +0800

    增加jsonp支持

--- p.php
+++ p.php
@@ -6,7 +6,6 @@
  * var hq_str_sz300212="易华录,33.010,32.030,32.760,33.490,32.600,32.760,32.800,6261759,206925190.880,900,32.760,4100,32.750,4600,32.740,4240,32.730,20900,32.720,500,32.800,700,32.810,1800,32.820,1000,32.870,200,32.880,2016-02-22,11:35:56,00";
  */
 
-header('Content-Type: application/json');
 $response = array();
 if (isset($_GET['s'])) {
     $s = trim($_GET['s']);
@@ -44,4 +43,10 @@ if (isset($_GET['s'])) {
         }
     }
 }
-echo json_encode($response);
+if (isset($_GET['jsonp']) && preg_match('/^[_a-zA-Z][A-Za-z0-9_]*/', $_GET['jsonp'])) {
+    header('Content-Type: application/javascript');
+    echo $_GET['jsonp'], '(', json_encode($response), ');';
+} else {
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
